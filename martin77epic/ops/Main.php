639f4ff5a67fb0c4948c98d6a2bfd9b84b167106
MrMojang <freac99zocker@gmail.com>
2016-06-30 13:29:25 +0200

    Create Main.php

---
+++ martin77epic/ops/Main.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace martin77epic\ops;
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\command\CommandExecutor;
+use pocketmine\event\Listener;
+use pocketmine\Player;
+use pocketmine\plugin\PluginBase;
+use pocketmine\Server;
+use pocketmine\utils\TextFormat;
+use pocketmine\utils\Config;
+use pocketmine\permission\ServerOperator;
+
+class Main extends PluginBase implements Listener{
+    
+    public function onEnable(){
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        $this->saveDefaultConfig();
+        $this->getLogger()->info(TextFormat::AQUA . "[OPs]"TextFormatt::DARK_GREEN . "enabled");
+    }
+    
+    public function onDisable(){
+        $this->getLogger()->info(TextFormat::AQUA . "[OPs]" disabled);
+    }
+    
+    public function onCommand(CommandSender $sender, Command $command, $label, array $args){
+        switch($command->getName()){
+        
+                 case "ops":
+                
+			$ops = "";
+			if($sender->hasPermission("m77e.ops")){
+				foreach($this->getServer()->getOnlinePlayers() as $p){
+					if($p->isOnline() && $p->isOp()){
+						$ops = $p->getName()." , ";
+						$sender->sendMessage(TextFormat::AQUA."OPs online:\n".substr($ops, 0, -2));		
+						return true;
+					}else{
+						$sender->sendMessage(TextFormat::AQUA."OPs online: \n");
+						return true;
+					}
+				}
+			}else{
+				$sender->sendMessage(TextFormat::RED."No Permission!");
+				return true;
+			}
+			
+			}
+	}
+}
